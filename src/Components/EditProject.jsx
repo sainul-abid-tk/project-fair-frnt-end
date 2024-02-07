9be6b2f86d6e18fe1a2bf6a1251daadc0b178066
sainul-abid-tk <sainulabidtk2468@gmail.com>
2024-02-07 18:19:40 +0530

    5th

--- src/Components/EditProject.jsx
+++ src/Components/EditProject.jsx
@@ -1,15 +1,80 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import {Modal,Button} from 'react-bootstrap'
 import uploadImage from '../assets/images/uploadimage.png'
+import { SERVER_URL } from '../Services/serverUrl';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function EditProject() {
+function EditProject({project}) {
+  console.log(project);
   const [show, setShow] = useState(false);
+  const [projectData,setProjectData]=useState({
+    id:project._id,
+    title:project.title,
+    language:project.languages,
+    overview:project.overview,
+    github:project.github,
+    website:project.website,
+    projectImage:""
+  })
+  const [previewImage,setPreviewImage]=useState("")
+  useEffect(()=>{
+    if(projectData.projectImage){
+      setPreviewImage(URL.createObjectURL(projectData.projectImage))
+    }else{
+      setPreviewImage("")
+    }
 
+  },[projectData.projectImage])
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleShow = () => {
+    setShow(true)
+    setProjectData({
+      id:project._id,
+    title:project.title,
+    language:project.languages,
+    overview:project.overview,
+    github:project.github,
+    website:project.website,
+    projectImage:""
+    })
+    setPreviewImage("")
+  }
+  const handleUpdate=()=>{
+    const {title,language,overview,github,website,projectImage}=projectData
+    if(!title || !language || !overview || !github || !website){
+      toast.warning("please fill the form completely!!!")
+    }else{
+      // Api call
+      const reqBody=new FormData()
+      reqBody.append("title",title)
+      reqBody.append("languages",language)
+      reqBody.append("overview",overview)
+      reqBody.append("github",github)
+      reqBody.append("website",website)
+      previewImage?reqBody.append("projectImage",projectImage):reqBody.append("projectImage",project.projectImage)
+      // api call header
+      const token =sessionStorage.getItem("token")
+      if(token){
+        if(previewImage){
+          const reqHeader={
+            "Content-Type":"multipart/form-data",
+            "Authorization":`Bearer ${token}`
+          }
+          // api call
+        }else{
+          const reqHeader={
+            "Content-Type":"application/json",
+            "Authorization":`Bearer ${token}`
+          }
+          // api call
+        }
+    }
+  }
+}
   return (
     <>
-    
+    <ToastContainer autoClose={3000} theme="colored" position="top-center"/>
     <button onClick={handleShow} className='btn'><i className='fa-solid fa-edit fa-2x overflow-hidden '></i></button>
     <Modal size='lg'
         show={show}
@@ -25,29 +90,29 @@ function EditProject() {
           <div className="row">
             <div className="col-lg-6">
             <label className="text-center">
-              <input style={{ display: "none" }} type="file" />
+              <input style={{ display: "none" }} type="file" onChange={e=>setProjectData({...projectData,projectImage:e.target.files[0]})}/>
               <img
                 className='w-100'
-                src={uploadImage}
+                src={previewImage?previewImage:`${SERVER_URL}/uploads/${project.projectImage}`}
                 alt="upload image"
               />
             </label>
             </div>
             <div className="col-lg-6">
               <div className='mb-3'>
-                  <input type="text" className='form-control' placeholder='Project Title'/>
+                  <input value={projectData.title} onChange={e=>setProjectData({...projectData,title:e.target.value})} type="text" className='form-control' placeholder='Project Title'/>
               </div>
               <div className='mb-3'>
-                  <input type="text" className='form-control' placeholder='language Used'/>
+                  <input value={projectData.language} onChange={e=>setProjectData({...projectData,language:e.target.value})} type="text" className='form-control' placeholder='language Used'/>
               </div>
               <div className='mb-3'>
-                  <input type="text" className='form-control' placeholder='Project Github Link'/>
+                  <input value={projectData.github} onChange={e=>setProjectData({...projectData,github:e.target.value})} type="text" className='form-control' placeholder='Project Github Link'/>
               </div>
               <div className='mb-3'>
-                  <input type="text" className='form-control' placeholder='Project Website Link'/>
+                  <input value={projectData.website} onChange={e=>setProjectData({...projectData,website:e.target.value})} type="text" className='form-control' placeholder='Project Website Link'/>
               </div>
               <div className='mb-3'>
-                  <input type="text" className='form-control' placeholder='Project Overview'/>
+                  <input value={projectData.overview} onChange={e=>setProjectData({...projectData,overview:e.target.value})} type="text" className='form-control' placeholder='Project Overview'/>
               </div>
             </div>
           </div>
@@ -56,7 +121,7 @@ function EditProject() {
           <Button variant="secondary" onClick={handleClose}>
             Cancel
           </Button>
-          <Button variant="primary">Add</Button>
+          <Button variant="primary" onClick={handleUpdate}>Update</Button>
         </Modal.Footer>
       </Modal>
       </>
